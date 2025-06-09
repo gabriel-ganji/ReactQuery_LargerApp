d2192d8219e8d5e551c1d702fc2a2c73dc717186
gabriel-ganji <gabrielganji01011010@gmail.com>
2025-06-09 11:50:21 -0300

    feat: selector for useStaff

--- client/src/components/staff/hooks/useStaff.ts
+++ client/src/components/staff/hooks/useStaff.ts
@@ -1,11 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 import type { Staff } from "@shared/types";
 
 // import { filterByTreatment } from "../utils";
 import { axiosInstance } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
+import { filterByTreatment } from "../utils";
 
 // query function for useQuery
 async function getStaff(): Promise<Staff[]> {
@@ -16,11 +17,21 @@ async function getStaff(): Promise<Staff[]> {
 export function useStaff() {
   // for filtering staff by treatment
   const [filter, setFilter] = useState("all");
+
+  const selectFn = useCallback(
+    (unfilteredStaff: Staff[]) => {
+      if(filter === "all") return unfilteredStaff;
+      return filterByTreatment(unfilteredStaff, filter);
+    },
+    [filter]
+  );
+
   const fallback: Staff[] = [];
 
   const { data: staff = fallback } = useQuery({
     queryKey: [queryKeys.staff],
     queryFn: getStaff,
+    select: selectFn,
   });
 
   return { staff, filter, setFilter };
